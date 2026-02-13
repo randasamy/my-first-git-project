c8417baf64f0856b7e70748a0dcc2fdb224fc25d
randasamy <randasamy@gmail.com>
2026-02-13 14:26:05 +0100

    Add greet function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+void greet() {
+    std::cout << "This is a new feature!" << std::endl;
+}
+
 int main() {
     std::cout << "Hello, Git + C++ on Windows!" << std::endl;
+    greet();
     return 0;
 }
